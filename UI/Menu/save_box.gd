a1cded815cdf70d3df185c943821f8612c274359
IzzytehWolf <44793093+swapmasterx@users.noreply.github.com>
2025-11-13 20:19:19 -0700

    a

--- UI/Menu/save_box.gd
+++ UI/Menu/save_box.gd
@@ -59,16 +59,15 @@ func _on_save_pressed():
 func _input(event):
 	if save_pressed == true:
 		if event.is_action_pressed("interact_confirm"):
-			SignalManager.saved_menu_closed.emit()
 			save_pressed = false
 			close_save_box()
 
 func _on_cancel_pressed():
-	SignalManager.saved_menu_closed.emit()
+	
 	close_save_box()
 	
 func close_save_box():
-	
+	SignalManager.saved_menu_closed.emit()
 	self.visible = false
 	GlobalFlags.wasd_lock = false
 	player_soul.visible = false
